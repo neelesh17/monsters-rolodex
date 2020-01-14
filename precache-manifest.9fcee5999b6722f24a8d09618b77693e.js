779bc7893f102f9f1e8bc89ebcaafc67d8de8235
Neelesh <nt00605@gmail.com>
2020-01-14 09:39:43 +0530

    Updates

---
+++ precache-manifest.9fcee5999b6722f24a8d09618b77693e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed6b564f8b3f8a84a32b98ce085b9ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "56a04be6c087b917aef4",
+    "url": "/monsters-rolodex/static/css/main.230cbf6f.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56a04be6c087b917aef4",
+    "url": "/monsters-rolodex/static/js/main.348f1a89.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
